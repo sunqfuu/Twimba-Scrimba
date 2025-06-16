dbf3a335f12143729deb24a807282eefb566077b
Orhan Sağır <sagir.o@outlook.com>
2025-06-16 16:22:55 +0300

    Add reply display functionality to tweets
    Implements the display of tweet replies by:
    - Replacing console.log with proper HTML generation for replies
    - Adding a dedicated replies container for each tweet
    - Including profile picture, handle, and tweet text for each reply
    
    This enhances the user experience by showing threaded conversations directly in the tweet feed instead of just logging them to console.

--- index.js
+++ index.js
@@ -57,8 +57,22 @@ function getFeedHtml() {
       retweetIconClass = "retweeted";
     }
 
+    let repliesHtml = "";
+
     if (tweet.replies.length > 0) {
-      console.log(tweet.replies);
+      tweet.replies.forEach(function (reply) {
+        repliesHtml += `
+                    <div class="tweet-reply">
+                        <div class="tweet-inner">
+                            <img src="${reply.profilePic}" class="profile-pic">
+                            <div>
+                                <p class="handle">${reply.handle}</p>
+                                <p class="tweet-text">${reply.tweetText}</p>
+                            </div>
+                        </div>
+                    </div>
+                `;
+      });
     }
 
     feedHtml += `
@@ -90,6 +104,9 @@ function getFeedHtml() {
             </div>   
         </div>            
     </div>
+    <div id="replies-${tweet.uuid}"">
+        ${repliesHtml}
+    </div>  
 </div>
 `;
   });
